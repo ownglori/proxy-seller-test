7e6f6e09ec3e6134860eea715b84dee533280857
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-19 22:06:04 +0200

    loader

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,6 +3,11 @@ import React from "react";
 
 export const Loading = () => {
   return (
-    <div>Loading...</div>
+    <div
+      className="bg-white position-fixed top-0 end-0 bottom-0 start-0 d-flex justify-content-center align-items-center">
+      <div className="spinner-border text-primary">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
   );
 };
